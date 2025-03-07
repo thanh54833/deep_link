e242d60b47e9ba08a79f3ec8872b2470629b5950
phamthanh <phamhoaithanh32@gmail.com>
2025-03-07 13:33:03 +0700

    update#

--- android/app/src/main/kotlin/com/example/deep_link/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/deep_link/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.deep_link
+package com.concung.ecapp
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/webview_main.dart
+++ lib/webview_main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,7 +20,7 @@ class WebViewExample extends StatefulWidget {
 }
 
 class _WebViewExampleState extends State<WebViewExample> {
-  // late WebViewController controller;
+  late WebViewController controller;
 
   // Future<void> _handleDeepLink(String url) async {
   //   // Parse the URL and navigate to the appropriate page in your app
@@ -33,47 +33,51 @@ class _WebViewExampleState extends State<WebViewExample> {
     // TODO: implement initState
     super.initState();
 
-    // controller = WebViewController()
-    //   ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    //   ..setNavigationDelegate(
-    //     NavigationDelegate(
-    //       onPageStarted: (String url) {},
-    //       onPageFinished: (String url) {},
-    //       onWebResourceError: (WebResourceError error) {},
-    //       onNavigationRequest: (NavigationRequest request) {
-    //         print("request .. ${request.url}");
-    //         return NavigationDecision.navigate;
-    //       },
-    //     ),
-    //   )
-    //   // https://vn.shp.ee/cWaHzBA
-    //   // https://dl.thanh-1122.duckdns.org
-    //   // https://vn.shp.ee/cWaHzBA
-    // // https://shopee.vn/product/363428261/23662909925?d_id=f574b&uls_trackid=52529r2q00rj&utm_content=1111DpHNwp9t7g7vVb8cgrzrsq
-    //   ..loadRequest(Uri.parse('https://dl.thanh-1122.duckdns.org'));
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            print("request .. ${request.url}");
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      // https://vn.shp.ee/cWaHzBA
+      // https://dl.thanh-1122.duckdns.org
+      // https://vn.shp.ee/cWaHzBA
+      // https://shopee.vn/product/363428261/23662909925?d_id=f574b&uls_trackid=52529r2q00rj&utm_content=1111DpHNwp9t7g7vVb8cgrzrsq
+      ..loadRequest(Uri.parse(
+          'https://shopee.vn/product/363428261/23662909925?d_id=f574b&uls_trackid=52529r2q00rj&utm_content=1111DpHNwp9t7g7vVb8cgrzrsq'));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: InkWell(
-          onTap: () async {
-            await launchUrl(
-              Uri.parse("https://concung.com/sua-bot/spdd-c-thuc-icreo-learning-milk-820g-70077.html"),
-              mode: LaunchMode.externalApplication,
-            );
-          },
-          child: Container(
-            height: 100,
-            width: 100,
-            color: Colors.red,
-          ),
+      // body: Center(
+      //   child: InkWell(
+      //     onTap: () async {
+      //       await launchUrl(
+      //         Uri.parse("https://concung.com/sua-bot/spdd-c-thuc-icreo-learning-milk-820g-70077.html"),
+      //         mode: LaunchMode.externalApplication,
+      //       );
+      //     },
+      //     child: Container(
+      //       height: 100,
+      //       width: 100,
+      //       color: Colors.red,
+      //     ),
+      //   ),
+      // ),
+      body: Container(
+        margin: const EdgeInsets.only(top: 50),
+        child: WebViewWidget(
+          controller: controller,
         ),
       ),
-      // body: WebViewWidget(
-      //   controller: controller,
-      // ),
     );
   }
 }
